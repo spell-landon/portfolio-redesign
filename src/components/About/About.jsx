f472decb30de675409fcbfba0554944e820479b2
spell-landon <spell.landon@gmail.com>
2022-03-10 18:28:09 -0600

    Add motion to About sections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -8,8 +8,13 @@ function About(props) {
   const scrollRef = useRef(null);
   return (
     <Element id='about' name='about'>
-      <div className={styles.about}>
-        <h3>a bit about me...</h3>
+      <motion.div className={styles.about} layout>
+        <motion.h3
+          initial={{ opacity: 0, x: -25 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ ease: 'easeInOut', duration: 1, delay: 0.5 }}>
+          a bit about me...
+        </motion.h3>
         <motion.div
           className={styles.gridContainer}
           ref={scrollRef}
@@ -20,16 +25,24 @@ function About(props) {
             whileInView={{ opacity: 1, translateY: 0 }}
             viewport={{ amount: 0.5 }}
             transition={{ ease: 'easeIn', duration: 0.3 }}></motion.div>
-          <div>
-            <section>
+          <motion.div>
+            <motion.section
+              initial={{ opacity: 0, translateX: 100 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              viewport={{ amount: 0.5 }}
+              transition={{ ease: 'easeIn' }}>
               <h4>Education</h4>
               <p>
                 I am a recent graduate of the Software Engineering Program at
                 General Assembly where I focused on different languages and
                 frameworks.
               </p>
-            </section>
-            <section>
+            </motion.section>
+            <motion.section
+              initial={{ opacity: 0, translateX: 100 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              viewport={{ amount: 0.5 }}
+              transition={{ ease: 'easeIn' }}>
               <h4>History</h4>
               <p>
                 Prior to the program, I was an Estimator and Project Manager for
@@ -41,8 +54,12 @@ function About(props) {
                 same level of optimism, gumption and dedication towards a
                 Software Development role.
               </p>
-            </section>
-            <section>
+            </motion.section>
+            <motion.section
+              initial={{ opacity: 0, translateX: 100 }}
+              whileInView={{ opacity: 1, translateX: 0 }}
+              viewport={{ amount: 0.5 }}
+              transition={{ ease: 'easeIn' }}>
               <h4>Motto?</h4>
               <p>
                 My motto is “we'll figure it out”, because if there's a problem
@@ -50,10 +67,10 @@ function About(props) {
                 that needs to be implemented, we can, and will, figure it out
                 and get the job done!
               </p>
-            </section>
-          </div>
+            </motion.section>
+          </motion.div>
         </motion.div>
-      </div>
+      </motion.div>
     </Element>
   );
 }
